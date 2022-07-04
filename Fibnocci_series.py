fd2052dc17e467bc2a80d5a2b237819e6f1dd6a3
Dedeepya5105 <77732444+Dedeepya5105@users.noreply.github.com>
2022-07-04 19:56:07 +0530

    Create Fibnocci_series.py
    Fibonacci series
    In the Fibonacci series, the First 2 numbers will be constant, which are 0,1.
    Then the next number will be the sum of the previous 2 numbers.

---
+++ Fibnocci_series.py
@@ -0,0 +1,13 @@
+#Fibonocci series code
+
+first = 0
+second = 1
+
+print(first)
+print(second)
+
+for i in range(2,10):
+    next = first + second
+    first = second
+    second = next
+    print(next)
